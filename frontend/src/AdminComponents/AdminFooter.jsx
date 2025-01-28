81a50b8de948b8389e3032eb16e032e4bf5b8200
palmergk <palmergid@gmail.com>
2025-01-28 20:18:29 +0100

    updates

--- frontend/src/AdminComponents/AdminFooter.jsx
+++ frontend/src/AdminComponents/AdminFooter.jsx
@@ -1,38 +1,62 @@
-import React from 'react'
-import { MdCurrencyExchange } from "react-icons/md";
+import React, { useState } from 'react'
+import { MdCurrencyExchange, MdLeaderboard } from "react-icons/md";
 import { BsGiftFill } from "react-icons/bs";
 import { CgToolbox } from "react-icons/cg";
 import { Link, useLocation } from 'react-router-dom';
 import { FaUser } from "react-icons/fa";
+import { RiSettings5Fill } from 'react-icons/ri'
 import { MoveToTop } from '../utils/pageUtils';
+import { IoNotificationsSharp } from 'react-icons/io5';
+import { CiMenuKebab } from 'react-icons/ci';
+
+const mainIcons = [
+    {
+        name: 'crypto exchange',
+        symbol: MdCurrencyExchange,
+        url: '/admin/exchange'
+    },
+    {
+        name: 'gift cards',
+        symbol: BsGiftFill,
+        url: '/admin/giftcards'
+    },
+    {
+        name: 'profit tools',
+        symbol: CgToolbox,
+        url: '/admin/profit_tools'
+    },
+    {
+        name: 'settings',
+        symbol: RiSettings5Fill,
+        url: '/admin/settings'
+    },
+    {
+        name: 'profile',
+        symbol: FaUser,
+        url: '/admin/profile'
+    },
+]
+
+const extraIcons = [
+    {
+        name: 'notifications',
+        symbol: IoNotificationsSharp,
+        url: '/admin/notifications',
+    },
+    {
+        name: 'leaderboard',
+        symbol: MdLeaderboard,
+        url: '/admin/leaderboard',
+    },
+]
+
+const extraArray = [
+    '/admin/notifications', '/admin/leaderboard'
+]
 
 
 const AdminFooter = () => {
-
-    const icons = [
-        {
-            name: 'crypto exchange',
-            symbol: MdCurrencyExchange,
-            url: '/user/exchange'
-        },
-        {
-            name: 'gift cards',
-            symbol: BsGiftFill,
-            url: '/user/giftcards'
-        },
-        {
-            name: 'profit tools',
-            symbol: CgToolbox,
-            main: 'user/profit_tools',
-            url: '/user/profit_tools/create'
-        },
-        {
-            name: 'profile',
-            symbol: FaUser,
-            url: '/user/profile'
-        },
-    ]
-
+    const [view, setView] = useState(false)
     const location = useLocation()
     const pathName = location.pathname
     const active = 'text-lightgreen'
@@ -41,20 +65,45 @@ const AdminFooter = () => {
 
     return (
         <div className='w-full fixed bottom-0 z-50'>
-            <div className="w-full mx-auto px-5 flex relative items-center bg-[#212134]  justify-around gap-2">
-                {icons.map((item, i) => {
-                    return (
-                        <div key={i} className="flex items-center pt-5 pb-3 relative" >
-                            {pathName === item.url &&
-                                <div className="bg-lightgreen absolute top-0 w-full h-1 rounded-b-full "></div>
-                            }
-                            <Link to={item.url} onClick={MoveToTop}
-                                className={` group-hover:text-lightgreen px-2  ${pathName === item.url ? active : nonactive} cursor-pointer flex items-center flex-col gap-1"`}>
-                                <div className="text-[1.5rem]  ">{<item.symbol />}</div>
-                            </Link>
+            <div className='w-11/12 mx-auto relative'>
+                <div className="px-5 flex relative items-center bg-[#212134] border border-secondary rounded-full justify-around gap-2">
+                    {mainIcons.map((item, i) => {
+                        return (
+                            <div key={i} className="flex items-center py-4 relative" >
+                                {pathName === item.url &&
+                                    <div className="bg-lightgreen absolute top-0 w-full h-1 rounded-b-full "></div>
+                                }
+                                <Link to={item.url} onClick={MoveToTop}
+                                    className={` group-hover:text-lightgreen px-2  ${pathName === item.url ? active : nonactive} cursor-pointer flex flex-col gap-1 items-center`}>
+                                    <div className="text-[1.5rem]">{<item.symbol />}</div>
+                                </Link>
+                            </div>
+                        )
+                    })}
+                    <div className='flex items-center py-4 relative'>
+                        {extraArray.includes(pathName) &&
+                            <div className="bg-lightgreen absolute top-0 w-full h-1 rounded-b-full "></div>
+                        }
+                        <div className={`group-hover:text-lightgreen px-2 cursor-pointer text-[1.5rem] ${extraArray.includes(pathName) ? active : nonactive}`} onClick={() => setView(!view)}>
+                            <CiMenuKebab />
                         </div>
-                    )
-                })}
+                    </div>
+                </div>
+                {view &&
+                    <div className='absolute -top-12 right-0 bg-secondary px-4 flex items-center justify-around gap-2 rounded-full'>
+                        {extraIcons.map((item, i) => (
+                            <div key={i} className='flex items-center py-4 relative'>
+                                {pathName === item.url &&
+                                    <div className="bg-lightgreen absolute top-0 w-full h-1 rounded-b-full"></div>
+                                }
+                                <Link to={item.url} onClick={MoveToTop}
+                                    className={` group-hover:text-lightgreen px-2  ${pathName === item.url ? active : nonactive} cursor-pointer flex flex-col gap-1 items-center`}>
+                                    <div className="text-[1.5rem]">{<item.symbol />}</div>
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
+                }
             </div>
         </div>
     )
--- frontend/src/AuthComponents/AuthFooter.jsx
+++ frontend/src/AuthComponents/AuthFooter.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { MdCurrencyExchange } from "react-icons/md";
+import React, { useState } from 'react'
+import { MdCurrencyExchange, MdLeaderboard } from "react-icons/md";
 import { BsGiftFill } from "react-icons/bs";
 import { MdDashboard } from "react-icons/md";
 import { CgToolbox } from "react-icons/cg";
@@ -8,77 +8,122 @@ import { GoHistory } from "react-icons/go";
 import { FaUser } from "react-icons/fa";
 import { MoveToTop } from '../utils/pageUtils';
 import { CiMenuKebab } from "react-icons/ci";
+import { IoNotificationsSharp } from 'react-icons/io5';
+import { TbBuildingBank } from "react-icons/tb";
+
+const mainIcons = [
+    {
+        name: 'dashboard',
+        symbol: MdDashboard,
+        url: '/user/dashboard'
+    },
+    {
+        name: 'crypto exchange',
+        symbol: MdCurrencyExchange,
+        url: '/user/exchange'
+    },
+    {
+        name: 'gift cards',
+        symbol: BsGiftFill,
+        url: '/user/giftcards'
+    },
+    {
+        name: 'profit tools',
+        symbol: CgToolbox,
+        main: 'user/profit_tools',
+        url: '/user/profit_tools/create'
+    },
+    {
+        name: 'bank withdrawal',
+        symbol: TbBuildingBank,
+        url: '/user/bank_withdrawal'
+    },
+]
+
+const extraIcons = [
+    {
+        name: 'transaction history',
+        symbol: GoHistory,
+        url: '/user/transactions_history'
+    },
+    {
+        name: 'profile',
+        symbol: FaUser,
+        main: 'user/profile',
+        url: '/user/profile'
+    },
+    {
+        name: 'notifications',
+        symbol: IoNotificationsSharp,
+        url: '/user/notifications',
+    },
+    {
+        name: 'leaderboard',
+        symbol: MdLeaderboard,
+        url: '/user/leaderboard',
+    },
+]
+
+const extraArray = [
+    '/user/transactions_history', '/user/profile', '/user/profile/kyc', '/user/notifications', 'user/leaderboard'
+]
 
 
 const AuthFooter = () => {
-
-    const icons = [
-        {
-            name: 'dashboard',
-            symbol: MdDashboard,
-            url: '/user/dashboard'
-        },
-        {
-            name: 'crypto exchange',
-            symbol: MdCurrencyExchange,
-            url: '/user/exchange'
-        },
-        {
-            name: 'gift cards',
-            symbol: BsGiftFill,
-            url: '/user/giftcards'
-        },
-        {
-            name: 'profit tools',
-            symbol: CgToolbox,
-            main: 'user/profit_tools',
-            url: '/user/profit_tools/create'
-        },
-        {
-            name: 'profile',
-            symbol: FaUser,
-            url: '/user/profile'
-        },
-        
-        {
-            name: 'more',
-            symbol: CiMenuKebab ,
-            url: '/user/transactions_history'
-        }
-    ]
-
-    const extras = [
-        {
-            name: 'transaction history',
-            symbol: GoHistory,
-            url: '/user/transactions_history'
-        },
-    ]
+    const [view, setView] = useState(false)
     const location = useLocation()
     const pathName = location.pathname
-    const active = 'text-lightgreen' 
+    const active = 'text-lightgreen'
     const nonactive = 'text-white/60 hover:text-lightgreen'
-    
+
 
     return (
         <div className='w-full fixed bottom-0 z-50'>
-            <div className="w-full mx-auto px-5 flex relative items-center bg-[#212134]  justify-around gap-2">
-                {icons.map((item, i) => {
-                    return (
-                        <div key={i} className="flex items-center pt-5 pb-3 relative" >
-                            {item.name === 'profit tools' ? pathName.includes(item.main) && 
-                                <div className="bg-lightgreen absolute top-0 w-full h-1 rounded-b-full "></div>
-                                :
-                                pathName === item.url &&
-                                <div className="bg-lightgreen absolute top-0 w-full h-1 rounded-b-full "></div>
-                            }
-                            <Link to={item.url} onClick={MoveToTop}
-                                className={` group-hover:text-lightgreen px-2  ${item.name === 'profit tools' ? pathName.includes(item.main) ? active : nonactive : pathName === item.url ? active : nonactive} cursor-pointer flex items-center flex-col gap-1"`}>
-                                <div className="text-[1.5rem]  ">{<item.symbol />}</div>
-                            </Link>
+            <div className='w-11/12 mx-auto relative'>
+                <div className="w-full px-5 flex relative items-center bg-[#212134] border border-secondary  justify-around gap-2 rounded-full">
+                    {mainIcons.map((item, i) => {
+                        return (
+                            <div key={i} className="flex items-center py-4 relative" >
+                                {item.name === 'profit tools' ? pathName.includes(item.main) &&
+                                    <div className="bg-lightgreen absolute top-0 w-full h-1 rounded-b-full "></div>
+                                    :
+                                    pathName === item.url &&
+                                    <div className="bg-lightgreen absolute top-0 w-full h-1 rounded-b-full "></div>
+                                }
+                                <Link to={item.url} onClick={MoveToTop}
+                                    className={` group-hover:text-lightgreen px-2  ${item.name === 'profit tools' ? pathName.includes(item.main) ? active : nonactive : pathName === item.url ? active : nonactive} cursor-pointer flex flex-col gap-1 items-center`}>
+                                    <div className="text-[1.5rem]">{<item.symbol />}</div>
+                                </Link>
+                            </div>
+                        )
+                    })}
+                    <div className='flex items-center py-4 relative'>
+                        {extraArray.includes(pathName) &&
+                            <div className="bg-lightgreen absolute top-0 w-full h-1 rounded-b-full "></div>
+                        }
+                        <div className={`group-hover:text-lightgreen px-2 cursor-pointer text-[1.5rem] ${extraArray.includes(pathName) ? active : nonactive}`} onClick={() => setView(!view)}>
+                            <CiMenuKebab />
                         </div>
-                    )
-                })}
+                    </div>
+                </div>
+                {view &&
+                    <div className='absolute -top-12 right-0 bg-secondary px-4 flex items-center justify-around gap-2 rounded-full'>
+                        {extraIcons.map((item, i) => (
+                            <div key={i} className='flex items-center py-4 relative'>
+                                {item.name === 'profile' ? pathName.includes(item.main) &&
+                                    <div className="bg-lightgreen absolute top-0 w-full h-1 rounded-b-full "></div>
+                                    :
+                                    pathName === item.url &&
+                                    <div className="bg-lightgreen absolute top-0 w-full h-1 rounded-b-full "></div>
+                                }
+                                <Link to={item.url} onClick={MoveToTop}
+                                    className={` group-hover:text-lightgreen px-2  ${item.name === 'profile' ? pathName.includes(item.main) ? active : nonactive : pathName === item.url ? active : nonactive} cursor-pointer flex flex-col gap-1 items-center`}>
+                                    <div className="text-[1.5rem]">{<item.symbol />}</div>
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
+                }
             </div>
         </div>
     )
