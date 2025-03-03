465101a6ed7cd5e18144066ff440e8ee3cca1ce2
Litezy <liteb237@gmail.com>
2025-03-03 21:18:45 +0100

    changes

--- frontend/src/GeneralComponents/Loader.jsx
+++ frontend/src/GeneralComponents/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = ({ title }) => {
   return (
     <div className="w-full h-full absolute top-0 left-0 z-50 flex flex-col gap-1 items-center justify-center bg-dark/30 backdrop-blur-sm">
       <div className=' scrollHide'>
-        <img src={logo} className='w-20 h-20 loaderimg' alt="" />
+        <img src={logo} className='w-20 h-20 animate-bounce' alt="" />
       </div>
       {title && <div className='text-white'>...{title}</div>}
     </div>
