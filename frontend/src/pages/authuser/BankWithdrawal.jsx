d57c9352fa4f6e9f4246df5ec1df4db28e733d2a
palmergk <palmergid@gmail.com>
2025-03-03 23:25:50 +0100

    updates

--- frontend/src/pages/authuser/BankWithdrawal.jsx
+++ frontend/src/pages/authuser/BankWithdrawal.jsx
@@ -11,7 +11,7 @@ import SelectComp from '../../GeneralComponents/SelectComp'
 import { useAtom } from 'jotai'
 import { BANK, UTILS, WALLET } from '../../services/store'
 import { Apis, AuthGetApi, AuthPostApi } from '../../services/API'
-import WithdrawComp from '../../AuthComponents/WIthdrawComp'
+import WithdrawComp from '../../AuthComponents/WithdrawComp'
 import { Link } from 'react-router-dom'
 
 
@@ -189,7 +189,7 @@ const formsal = () => {
                         <div className="w-full bg-primary p-5">
                             <div className="flex items-center flex-col lg:flex-row w-full justify-between  mb-5">
                                 <div className="text-xl lg:text-3xl font-bold text-gray-300  ">Request Withdrawal</div>
-                                <div className="text-sm text-red-600">minimum of {currencies[1].symbol}{utils.bank_withdraw_min.toLocaleString()} to initiate withdrawal</div>
+                                <div className="text-sm text-red-600">minimum of {currencies[1].symbol}{Object.values(utils).length !== 0 && utils.bank_withdraw_min.toLocaleString()} to initiate withdrawal</div>
                             </div>
                             <div onClick={prefillBank} className="mb-5 w-fit px-5 py-2 rounded-md cursor-pointer bg-ash text-white">Use linked account</div>
                             <div className="grid md:grid-cols-2 grid-cols-1 gap-5 lg:gap-10 mb-5">
