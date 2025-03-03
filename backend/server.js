c51fae64ca1b69cca2de31a1e64637291ea5b040
palmergk <palmergid@gmail.com>
2025-03-04 00:42:59 +0100

    updates

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT
 const server = http.createServer(app)
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174','https://moniequest.vercel.app/']
+    origin: ['http://localhost:5173', 'http://localhost:5174','https://moniequest.vercel.app']
 }))
 app.use(express.json())
 app.use(fileUpload())
