368dd830f6fadbbb7ddf470f0ec80b5fe9bc0da0
palmergk <palmergid@gmail.com>
2025-04-22 02:59:57 +0100

    updates

--- backend/routes/paystackRoutes.js
+++ backend/routes/paystackRoutes.js
@@ -1,13 +1,13 @@
 const { UserMiddleware, SuperAdminMiddleware } = require('../middleware/auth')
-const { handleWebhook, InitializeCryptoBuyPayment, getBanksFromPayStack, adminTransfer, checkAccount, InitializeProductBuyPayment} = require('../controllers/paystackController')
+const { handleWebhook, InitializeCryptoBuyPayment, getBanksFromPayStack, adminTransfer, checkAccount, InitializeProductBuyPayment } = require('../controllers/paystackController')
 
 const router = require('express').Router()
 
 router.post('/webhook', handleWebhook);
 router.get('/get_paystack_banks', getBanksFromPayStack);
-router.post('/initialize_buy_payment',UserMiddleware,InitializeCryptoBuyPayment)
-router.post('/admin_transfer',SuperAdminMiddleware,adminTransfer)
-router.post('/initialize_product_purchase',InitializeProductBuyPayment)
-router.get('/check_account',checkAccount)
+router.post('/initialize_buy_payment', UserMiddleware, InitializeCryptoBuyPayment)
+router.post('/admin_transfer', SuperAdminMiddleware, adminTransfer)
+router.post('/initialize_product_purchase', InitializeProductBuyPayment)
+router.get('/check_account', checkAccount)
 
 module.exports = router
